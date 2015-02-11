4f95dd0163295fbbaf213d90384bd31f6f207308
Your Name <you@example.com>
2015-02-12 00:00:02 +0100

    Added TODO, and example to bootmgmt.pl

--- bin/bootmgmt.pl
+++ bin/bootmgmt.pl
@@ -15,6 +15,7 @@ use ExecuteAndTrace;
 # Example: 
 
 #  clear; sudo ./bootmgmt.pl add --mac 080027C4D9ED --distro ubuntu --release 1404 --arch x86_64
+#  sudo /opt/OPSbst/bin/bootmgmt.pl update --mac 00:20:75:1c:ad:67 --distro fedora --release 20 --arch x86_64 --media nfs --role kvm_host
 #  clear; sudo ./bootmgmt.pl add --mac 080027600349 --distro fedora --release 20 --arch x86_64 --role yumdownload
 #  clear; sudo ./bootmgmt.pl update --mac 080027346180 --distro fedora --release 20 --arch x86_64 --role vagrant
 #  clear; sudo ./bootmgmt.pl add --mac 080027346180 --distro fedora --release 19 --arch x86_64
