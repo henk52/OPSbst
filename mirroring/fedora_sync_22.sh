dbf422590cfc3eba01461b443a99a7a15e864c72
Your Name <you@example.com>
2015-12-29 01:22:05 +0100

    Update the mirror scripts.

--- mirroring/fedora_sync_22.sh
+++ mirroring/fedora_sync_22.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-rsync -vaH --filter=". fedora_filters_22.txt"  --numeric-ids --delete rsync://dl.fedoraproject.org/fedora-enchilada /var/ks/mirrors/f22
+rsync -vaH --filter=". fedora_filters_22.txt"  --numeric-ids --delete rsync://dl.fedoraproject.org/fedora-enchilada /var/ks/mirrors/fedora22
--- mirroring/fedora_sync_23.sh
+++ mirroring/fedora_sync_23.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# http://superuser.com/questions/156664/what-are-the-differences-between-the-rsync-delete-options
+# --delete-after: Receiver deletes after transfer, not before...If some other part of the rsync moved extra files elsewhere, you'd want this instead of --delete-delay, because --delete-delay decides what it's going to delete in the middle of transfer, whereas --delete-after checks the directory for files that should be deleted AFTER everything is finished.
 
-rsync -vaH --filter=". fedora_filters_23.txt"  --numeric-ids --delete rsync://dl.fedoraproject.org/fedora-enchilada /var/ks/mirrors/f23
+rsync -vaH --filter=". fedora_filters_23.txt"  --numeric-ids --delete-after rsync://dl.fedoraproject.org/fedora-enchilada /var/ks/mirrors/fedora23
